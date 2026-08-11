fef4dc3d77188e46ceb5ba7ee3ec1cdf10ecc1cd
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-11 11:08:50 +0200

    Setup

---
+++ script.js
@@ -0,0 +1,10 @@
+// Krav til oppgaven
+// For å sikre at du utfordrer deg selv, må prosjektet ditt inkludere en viss grad av kompleksitet, for eksempel:
+
+// Et API med flere ulike endepunkter som du bruker i prosjektet ditt.
+// Et API med et endepunkt som aksepterer parametere i URL-en.
+// API-data som må kjøres gjennom en løkke for å hente/generere innhold.
+
+// API'S USED:
+// Scryfall - https://api.scryfall.com
+// MTGJSON - https://mtgjson.com/api/v5/
