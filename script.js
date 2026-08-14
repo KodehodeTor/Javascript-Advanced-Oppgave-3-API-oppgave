8cfb06d200f0e4e3db6531d97ceccb20105a31df
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-14 11:00:09 +0200

    Deleted old notes in script.js added new plans in bottom

--- script.js
+++ script.js
@@ -141,8 +141,8 @@ function createCard(cardInfo) {
 }
 
 //NOTES & FUTURE PLANS
-// Add card title over the cards
-// Align the cards!
 // Card saver (localStorage)
+// When click cards, have save option, go to Cardmarket option and select option (for mass additions of cards)
+
 // Need a box on the top where you can save your cards and follow pricing trends in cardmarket etc.
 // Delete individual cards from the save_box
