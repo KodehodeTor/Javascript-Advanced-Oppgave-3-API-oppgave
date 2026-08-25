458df903408e71cf63ad2f81acc5d45523a9b836
KodehodeTor <torjarand.26pg@gmail.com>
2026-08-25 13:14:54 +0200

    Removed half logic in createSavedCards.

--- hero.js
+++ hero.js
@@ -16,29 +16,33 @@ export function displayHero(card) {
 //append saved cards in hero section
 export function displaySavedCards() {
   if (!savedCardCont) return;
+
   savedCardCont.innerHTML = "";
+
   const savedCards = loadedSavedCards();
 
   //no saved cards
-  if (savedCards.lenght === 0) {
+  if (savedCards.length === 0) {
     savedCardCont.innerHTML = `<p class="no_saved_cards">No saved cards yet</p>`;
     return;
   }
   //create cards
   savedCards.forEach((card) => {
-    crateSavedCard(card);
+    createSavedCard(card);
   });
 
   //Only start carousel if enough cards
-  if (savedCards.lenght >= 2) {
+  if (savedCards.length >= 2) {
     startCarousel(savedCards);
   }
 }
 
 //Create saved card
-savedCards.forEach((card) => {
+function createSavedCard(card) {
   const element = document.createElement("div");
+
   element.className = "saved_card";
+
   element.innerHTML = `
      <p>${card.name}</p>
         <img src="${card.image}" alt="${card.name}">
@@ -46,7 +50,7 @@ savedCards.forEach((card) => {
         <button data-name="${card.name}" class="remove_btn">Remove</button>
         `;
   savedCardCont.appendChild(element);
-});
+}
 
 // Start carousel
 
@@ -57,11 +61,14 @@ function startCarousel(savedCards) {
   });
 
   const cards = savedCardCont.querySelectorAll(".saved_card");
-  const half = cards.lenght / 2;
+
   const firstCard = cards[0];
-  const secondSetFirstCard = cards[half];
+  const secondSetFirstCard = cards[savedCards.length];
 
   const distance = secondSetFirstCard.offsetLeft - firstCard.offsetLeft;
+
+  console.log("Cards:", cards.length);
+  console.log("Distance:", distance);
 
   savedCardCont.animate(
     [
